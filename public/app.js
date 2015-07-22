f8cf1255c573830846b70ec0db03539874dea748
Miantang <antisaber@gmail.com>
2015-07-22 11:34:25 +0800

    reset all

--- public/app.js
+++ public/app.js
@@ -1,18 +1,23 @@
-(function($) {
-    'use strict';
+(function($) 
+{
+  'use strict';
 
-  String.prototype.startWith = function (str) {
+  String.prototype.startWith = function (str) 
+  {
       var reg = new RegExp("^" + str);
       return reg.test(this);
   }
 
-  String.prototype.endWith = function (str) {
+  String.prototype.endWith = function (str) 
+  {
       var reg = new RegExp(str + "$");
       return reg.test(this);
   }
 
-  Date.prototype.pattern = function (fmt) {
-      var o = {
+  Date.prototype.pattern = function (fmt) 
+  {
+      var o = 
+      {
           "M+": this.getMonth() + 1, //月份         
           "d+": this.getDate(), //日         
           "h+": this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, //小时         
@@ -22,7 +27,8 @@
           "q+": Math.floor((this.getMonth() + 3) / 3), //季度         
           "S": this.getMilliseconds() //毫秒         
       };
-      var week = {
+      var week = 
+      {
           "0": "/u65e5",
           "1": "/u4e00",
           "2": "/u4e8c",
@@ -31,22 +37,27 @@
           "5": "/u4e94",
           "6": "/u516d"
       };
-      if (/(y+)/.test(fmt)) {
+      if (/(y+)/.test(fmt)) 
+      {
           fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
       }
-      if (/(E+)/.test(fmt)) {
+      if (/(E+)/.test(fmt)) 
+      {
           fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "/u661f/u671f" : "/u5468") : "") + week[this.getDay() + ""]);
       }
-      for (var k in o) {
-          if (new RegExp("(" + k + ")").test(fmt)) {
+      for (var k in o) 
+      {
+          if (new RegExp("(" + k + ")").test(fmt)) 
+          {
               fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
           }
       }
       return fmt;
   }
 
-  $.ajaxSetup({
-      cache: false,
+  $.ajaxSetup(
+  {
+      cache: true,
       statusCode: {
           406: function () {
               $("#msg").html("Error 406 请求速度过快！");
@@ -67,8 +78,10 @@
       }
     });
     
-  function appvm() {
+  function appvm() 
+  {
       var self = this;
+
       self.showuser = ko.observable(false);
       self.shownav = ko.observable(false);
       self.showuinfo = ko.observable(false);
@@ -76,6 +89,26 @@
       self.uid = ko.observable("");
       self.pwd = ko.observable("");
 
+      self.link_acuss = function () {
+          go("web/ac_uss_page.html");
+      };
+      self.link_acaddus = function () {
+          go("web/ac_addus_page.html");
+      };
+      self.link_account = function () {
+          go("web/ac_page.html");
+      };
+      self.link_user_center = function () {
+          go("web/center_page.html");
+          self.showback(false);
+      };
+      self.link_alldvs = function () {
+          go("web/dvs_page.html");
+      };
+      self.link_adddv = function () {
+          go("web/dvadd_page.html");
+      };
+      
       self.rembme = ko.observable(false);
       if (typeof (Storage) !== "undefined") {
           if (localStorage.getItem("uid") !== null) {
@@ -97,46 +130,25 @@
                     localStorage.clear();
                   self.uid("");
                   self.pwd("");
-                  $("#msg").html("账号已清除！");
-                  $('#my-prompt').modal('open');
               }
           } else {
               $("#msg").html("你的浏览器不支持此功能！");
               $('#my-prompt').modal('open');
           }
       });
-        
-        self.link_nav = function (){
-            go("web/nav_page.html");
-        }
-      self.link_acuss = function () {
-          go("web/ac_uss_page.html");
-      };
-      self.link_acaddus = function () {
-          go("web/ac_addus_page.html");
-      };
-      self.link_account = function () {
-          go("web/ac_page.html");
-      };
-      self.link_user_center = function () {
-          go("web/center_page.html");
-      };
-      self.link_alldvs = function () {
-          go("web/dvs_page.html");
-      };
+      
       self.link_accountEditPwd = function () {
           go("web/acpwd_page.html");
       };
-      self.link_adddv = function () {
-          go("web/dvadd_page.html");
-      };
-    self.link_user = function () {
-        if ($.AMUI.utils.cookie.get('uid') !== null) {
+      
+    self.link_user = function () 
+    {
+        if ($.AMUI.utils.cookie.get('uid') !== null) 
+        {
             $("#dialog").html("你确定想退出系统吗？");
             var $confirm = $('#exit-confirm');
             var confirm = $confirm.data('am.modal');
                 var onConfirm = function () {
-                $.AMUI.utils.cookie.set('ukey', null);
                 $.AMUI.utils.cookie.set('uid', null);
                 $.AMUI.utils.cookie.set('pwd', null);
                 window.location = "index.html";
@@ -155,17 +167,25 @@
             }
         }
     };
-      self.bt_login = function () {
-          if (self.uid() !== "" && self.pwd() !== "") {
+      self.bt_login = function () 
+      {
+          if (self.uid() !== "" && self.pwd() !== "") 
+          {
               $.AMUI.progress.start();
               $.ajax({
-                  url: "/v1.0/user?username=" + self.uid() + "&pass=" + self.pwd()
+                  type:"POST",
+                  url: "/index.php/userlogin",
+                  data:{'username' : self.uid(), 'pwd': self.pwd()}
               }).done(function (data) {
-                    if (data.ukey !== null) {
-                        $.AMUI.utils.cookie.set('ukey', data.ukey);
-                        $.AMUI.utils.cookie.set('uid', self.uid());
-                        $.AMUI.utils.cookie.set('pwd', self.pwd());
-                      if (data.userId === "admin") {
+                console.log(data);
+                    if (data) 
+                    {
+                       // $.AMUI.utils.cookie.set('ukey', data.ukey);
+                        $.AMUI.utils.cookie.set('uid', self.uid(), { expires: 7 });
+                        $.AMUI.utils.cookie.set('pwd', self.pwd(), { expires: 7 });
+                        $.AMUI.utils.cookie.set('data', $.parseJSON(data));
+                      if ($.parseJSON(data).username == "admin") 
+                      {
                           self.showuser(true);
                       };
                       self.showuinfo(true);
@@ -194,12 +214,13 @@
   {
       $("#render").load(url);
       $("#menu1").offCanvas('close');
-    }
+  }
 
-    window.onbeforeunload = function () {
-        $.AMUI.utils.cookie.set('ukey', null);
-        $.AMUI.utils.cookie.set('uid', null);
-        $.AMUI.utils.cookie.set('pwd', null);
+  window.onbeforeunload = function () 
+  {
+      $.AMUI.utils.cookie.set('ukey', null);
+      $.AMUI.utils.cookie.set('uid', null);
+      $.AMUI.utils.cookie.set('pwd', null);
   }
 
 })(jQuery);
